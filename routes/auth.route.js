c67e1fa104561cc2d1d56b6df81b709b91e18857
Shehryar_Kiyani <shehryar@xenex-media.com.au>
2024-05-26 15:02:36 +0500

    google auth

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { Signup,Signin } from "../controllers/auth.controller.js";
+import { Signup,Signin,GoogleAuth } from "../controllers/auth.controller.js";
 const router = Router()
 router.post('/signup',Signup)
 router.post('/login',Signin)
+router.post('/google',GoogleAuth)
 export default router
